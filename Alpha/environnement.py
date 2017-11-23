7e309b959566720c790a20ef64af960b35102820
kkuette <awakeproduction@hotmail.fr>
2017-11-23 08:49:14 +0100

    Added GUI + env
    Former-commit-id: 1920321eff233d2d5235575d8195657f2807caf0
    Former-commit-id: 35163c901806b9e585347c7b560699fcb7d46c3c
    Former-commit-id: a008e6a807d70d189cdc33c1e1fec2b0059d4ad4 [formerly eef75f230b1585e57357d573db79b7f3b2069bab]
    Former-commit-id: bfd1d96ccfcadf159f55dfcb5fa21467214a8206

--- Alpha/environnement.py
+++ Alpha/environnement.py
@@ -6,3 +6,20 @@ class environnement():
         self.data_path = "./dataset/"
         self.period = 10
 
+        self.stock_name = ""
+        self.episode_count = 100
+        self.contract_price = 5
+        self.max_order = 5
+        self.spread = 1
+        self.window_size = 20
+        self.total_profit = 0
+        self.reward = 0
+        self.profit = 0
+        self.buy_price = 0
+        self.sell_price = 0
+        self.pause = 0
+        self.corder = ""
+        self.cdata = 0
+        self.inventory = None
+        self.POS_BUY = -1
+        self.POS_SELL = -1
---
+++ Alpha/run.py
@@ -0,0 +1,3 @@
+from interface import *
+
+launch_interface()
