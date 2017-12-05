d2c1ae857692a94dcb0ef4ecf5eccb79ddf09ee0
kkuette <awakeproduction@hotmail.fr>
2017-12-05 16:42:33 +0100

    Last update of 0.1 version
    Former-commit-id: 857dfd0b82298dbbff3322f0ab5bfe6da7c2d975
    Former-commit-id: 6202f492ba55b452501d76f2eaa4b9bafedc0ce7
    Former-commit-id: 83af6ff8ab8bb461daca5b0aa20ba3fa6ccbb01d [formerly 1e0d6e15ac577fecb394704d73405f1897a579bc] [formerly 077221a700123f2e4bd90c01b9e5ef301dfeb9e1 [formerly e832c8989ce2bd102508e04dc034a0e63634bd6d]] [formerly 96d5af26960d30b464b6e40ec2c542be66776b92]
    Former-commit-id: 6c20f1379a8a38367e16b43e755b56bb4728ab66
    Former-commit-id: b0b70bd642bf9b7b7fc65a1ee0caaddd2413a721 [formerly 6e027f396d09491d00456ae6dc45fab1eb5d3d20]
    Former-commit-id: 2df5e4500699f45e3f98d0331300818bd54d5823

---
+++ Alpha/v0.1/GUI/historic_w.py
@@ -0,0 +1,2 @@
+from ttkthemes import themed_tk as tk
+import tkinter.ttk
---
+++ Alpha/v0.1/run.py
@@ -0,0 +1,5 @@
+from GUI.interface import *
+from tools.utils import *
+
+if __name__ == '__main__':
+    launch_interface()
