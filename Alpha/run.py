77ee7bc331dfd8d8e1ca3df09f3487a776adf16b
kkuette <awakeproduction@hotmail.fr>
2017-12-06 15:51:57 +0100

    Started 0.2 version with PyQt lib
    Former-commit-id: 12095c5a8a3710dd2be01c1692f90ff6eac0e413
    Former-commit-id: 7911af7284b4bfe72f7083c0dd8308a00b71e987
    Former-commit-id: 52bfc158a9137128d00bfdac35f4b2921cb479de [formerly c28a1843e9b2b66a4b3c2e37b55496188838e4bd] [formerly f94ce827cadef8ccd03980ea05ac3603f050a58d [formerly 2a0a3d0cf3594f36cec19dbedb861b82660f8ed0]] [formerly 8f66feaf5113ea578a9a9c5028e997d09af69ece]
    Former-commit-id: 006eee207000380c1a5f3561d63cc5b5b25f6cec
    Former-commit-id: a581217cce83e49b8f86bfde6ebea9bdc23f001c [formerly 566db4a14b5859b27dc7294cdf5462b3f01f3592]
    Former-commit-id: e84a3da7345889d38c6c5df564dbe96d36311687

--- Alpha/run.py
+++
@@ -1,5 +0,0 @@
-from GUI.interface import *
-from tools.utils import *
-
-if __name__ == '__main__':
-    launch_interface()
---
+++ Alpha/v0.2/GUI/overview_window.py
@@ -0,0 +1,61 @@
+from core.environnement import *
+
+from PyQt5.QtWidgets import QWidget, QPushButton, QLabel, QFrame, QGridLayout, QGroupBox, QHBoxLayout, QVBoxLayout
+
+class Overview_Window(QWidget):
+
+    def __init__(self, root, env):
+        super(QWidget, self).__init__(root)
+
+    '''
+    def Agent_Inventory_Init(self, env):
+
+    def Agent_Orders_Init(self, env):
+
+    def Data_Init(self, env):
+
+    def Agent_Winrate(self, env):
+
+    def Agent_Value_Init(self, env):
+
+    def Agent_Profit(self, env):
+
+    def Agent_Reward(self, env):
+
+    def Time_Init(self, env):
+
+    def Update_Overview(self, env):
+    '''
+        #Day mod
+
+
+
+        #Agent Values
+
+
+
+        #Inventory
+
+
+
+        #Orders Done
+
+
+
+        #Orders
+
+
+
+        #Data
+
+
+
+        #Profit
+
+
+
+        #Reward
+
+
+
+        #Time
