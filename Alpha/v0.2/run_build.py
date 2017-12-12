77d85b904eb6cc8461798d48fb598c8714bca8a4
kkuette <awakeproduction@hotmail.fr>
2017-12-12 15:39:32 +0100

    Added wallet box in GUI + Added risk managment + POS managment + exposure managment
    Former-commit-id: bd091696b940de5bf391212830281849c20a3147
    Former-commit-id: 01c6ec7dc980e60de1199ddd5e4377596b0346c9
    Former-commit-id: d0f0f1fe2a55428fdcc467ad4887e3e13ff49329 [formerly 16314b81c7442bdf4b00dede7ad4784b4873473d] [formerly 3f9f3a3dca6750cedd7de2e4d8c6a06b0e2194bb [formerly 75247db1441bfba07bb713e5a61ae2c07295a1d1]] [formerly e5c8cb4e15008d8c2f5fe553e3865aca98c56c78]
    Former-commit-id: 911cf391385a58a71e232798f4f5ee0faf3623f4
    Former-commit-id: ba4b127140602448bdda9769ce9b3f4124e36fd6 [formerly 5bb51386a402a11f53d3ba60f505440409286cb9]
    Former-commit-id: 41e1ec44f634ac3fe38e1198bfe9bb72b36791e4

---
+++ Alpha/v0.2/run_build.py
@@ -0,0 +1,10 @@
+from tools.indicators.build_indicators import *
+from tools.utils import *
+
+import pandas as pd
+
+if __name__ == '__main__':
+    row_path = "../dataset/DAX30/1M"
+    new_path = "./data/DAX30_full_wi.csv"
+    data = get_all_data(row_path)
+    data.to_csv(new_path, sep = ';', mode= 'w')
