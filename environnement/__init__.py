8ed127254361ebc155754373511f270e51e69870
kkuette <awakeproduction@hotmail.fr>
2017-12-19 17:33:28 +0100

    Cleaned some dirty things
    Former-commit-id: 3dfef01988c9c7d0ef47563821e54fb07ca2fa65
    Former-commit-id: ea8d148a9d5d66c2c8adcae68dcf24da8292d661
    Former-commit-id: 0fa94d1666402567dfb7a40bf4a486529e70e03d
    Former-commit-id: f29b366aeb2f666a067c48507869848fa3743d51 [formerly 147d99a731ddd9070b78f0633abd8b3940d6a858]
    Former-commit-id: 313dc4a4047e7c416777f43ae0a2dd781504d7dc

---
+++ environnement/__init__.py
@@ -0,0 +1,3 @@
+from .base_env import Environnement
+from .stock_env import Stock_Env
+from .eve_env import Eve_Env
---
+++ environnement/eve_env.py
@@ -0,0 +1,6 @@
+from .base_env import Environnement
+
+class Eve_Env(Environnement):
+
+    def __init__(self):
+        Environnement.__init__(self)
