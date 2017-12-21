60568c1542623498e6eb71d8779046f27d68d3bb
kkuette <awakeproduction@hotmail.fr>
2017-12-21 16:44:59 +0100

    Added qdarkstyle
    Former-commit-id: c31457009bcf59c1e719278203310bc0fcbd69de
    Former-commit-id: a74737d936bbbedf377112561d7f3b679f308ab8
    Former-commit-id: 6a2ff35c34b35c38c00ee04e464cce944bf03050 [formerly 2fe2aa43936afa2ac528fb9eed453a704c87cccf] [formerly 4abd019edf5148dd2af0a89b3df4edd05b93174d [formerly 833f01ba94a642f153ba797c54395348f8b6807b]] [formerly 70052f812adc8baa2b8bb1d2ff0308a2d02c0b6d]
    Former-commit-id: 8285220dc5d382406097c786d64b237753603080
    Former-commit-id: a6ad6f04e6b41e868922d993a3cf85b0ebef8df1 [formerly 9b5036c3f72c9d14d6e47143a7fc93fce73be0cb]
    Former-commit-id: c1c9156d7d5993b954925efc51a60abc825b1474

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
-from GUI import MainWindow
-
+from PyQt5 import QtGui
 from PyQt5.QtWidgets import QApplication
+
+import qdarkstyle
+
+from GUI import MainWindow
 
 import sys
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    app.setStyle('Fusion')
+    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
     launch = MainWindow()
     launch.show()
     sys.exit(app.exec_())
