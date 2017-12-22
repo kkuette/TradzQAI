d988e1711ad154d13046f49d8075a6e0a2fc90df
kkuette <awakeproduction@hotmail.fr>
2017-12-22 13:48:41 +0100

    Moved expReplay from all agents to Agent, and added DDPG agent (not working)
    Former-commit-id: 20e35edd40b626a96ef1f4a7237d150dfcdf2bb3
    Former-commit-id: fd627587358443f955fa179498aac5421060c610
    Former-commit-id: c4cc0221aa05e3d918ebbbffbc9e0e0ae62ed9aa
    Former-commit-id: 3e2ad3d6d2b2af7ec8abaf0d8f9e45c4a462c1ee [formerly 74bce0bdcd7b4424d71530103cf057d8a0e24de7]
    Former-commit-id: 535d3ca1d79749a5fecd223b14fa5812be08c1e3

--- agents/DDQN.py
+++ agents/DDQN.py
@@ -1,5 +1,6 @@
 import keras
-from keras.models import Sequential, load_model
+
+from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
 
@@ -12,21 +13,32 @@ from collections import deque
 
 from .agent import Agent
 
-class DQN(Agent):
+class DDQN(Agent):
 
     def __init__(self, state_size, env=None, is_eval=False, model_name=""):
-        self.name = "DQN"
+        self.name = "DDQN"
         Agent.__init__(self, state_size, env=env, is_eval=is_eval, model_name=model_name)
 
     def build_model(self):
         self._load_model()
-        if not self.model:
+        if self.model is None:
             self.model = self._model()
+            self.target_model = self._model()
+        else:
+            self.target_model = self._model()
+            self.update_target_model()
 
     def _model(self):
         model = Sequential()
         model.add(Dense(32, input_dim=self.state_size.shape[1], activation='relu'))
         model.add(Dense(32, activation='relu'))
-        model.add(Dense(self.action_size, activation="linear"))
+        model.add(Dense(self.action_size, activation='linear'))
         model.compile(loss="mse", optimizer=Adam(lr=self.learning_rate))
         return model
+
+    def update_target_model(self):
+        old = self.target_model.get_weights()
+        new = self.model.get_weights()
+
+        weights = [[self.update_rate * n + (1 - self.update_rate) * o for n, o in zip(new_w, old_w)] for new_w, old_w in zip(new, old)]
+        self.target_model.set_weights(weights)
--- agents/__init__.py
+++ agents/__init__.py
@@ -2,4 +2,5 @@ from .DQN import DQN
 from .DRQN import DRQN
 from .DDQN import DDQN
 from .DDRQN import DDRQN
+from .DDPG import DDPG
 from .agent import Agent
