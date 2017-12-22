53c99a4b1bdd7dcb7331c029a87b6b65dc2726ce
kkuette <awakeproduction@hotmail.fr>
2017-12-22 13:48:41 +0100

    Moved expReplay from all agents to Agent, and added DDPG agent (not working)
    Former-commit-id: 20e35edd40b626a96ef1f4a7237d150dfcdf2bb3
    Former-commit-id: fd627587358443f955fa179498aac5421060c610
    Former-commit-id: d988e1711ad154d13046f49d8075a6e0a2fc90df [formerly 535d3ca1d79749a5fecd223b14fa5812be08c1e3] [formerly 3e2ad3d6d2b2af7ec8abaf0d8f9e45c4a462c1ee [formerly 74bce0bdcd7b4424d71530103cf057d8a0e24de7]] [formerly c4cc0221aa05e3d918ebbbffbc9e0e0ae62ed9aa]
    Former-commit-id: cb06210eff8288d3123577612b36e3503964f5e5
    Former-commit-id: f5d3efff5005b5ccfaffb8f2e072f2c8c92f3e9d [formerly 5ded5528c5cf26f821ef0dfaea7fc4332afdb874]
    Former-commit-id: 42e02f2a6ae468c37e0103358f1cbe511bd800d8

--- agents/DDRQN.py
+++ agents/DDRQN.py
@@ -1,7 +1,7 @@
 import keras
+
 from keras.models import Sequential
-from keras.models import load_model
-from keras.layers import Dense, PReLU, CuDNNLSTM, Reshape
+from keras.layers import Dense, PReLU, CuDNNLSTM, Flatten, Reshape, Dropout
 from keras.optimizers import Adam
 
 import os
@@ -13,16 +13,20 @@ from collections import deque
 
 from .agent import Agent
 
-class DRQN(Agent):
+class DDRQN(Agent):
 
     def __init__(self, state_size, env=None, is_eval=False, model_name=""):
-        self.name = "DRQN"
+        self.name = "DDRQN"
         Agent.__init__(self, state_size, env=env, is_eval=is_eval, model_name=model_name)
 
     def build_model(self):
         self._load_model()
         if not self.model:
             self.model = self._model()
+            self.target_model = self._model()
+        else:
+            self.target_model = self._model()
+            self.update_target_model()
 
     def _model(self):
         model = Sequential()
@@ -30,6 +34,13 @@ class DRQN(Agent):
         model.add(Dense(32, activation='relu'))
         model.add(Reshape((1, 32)))
         model.add(CuDNNLSTM(128))
-        model.add(Dense(self.action_size, activation="linear"))
+        model.add(Dense(self.action_size, activation='linear'))
         model.compile(loss="mse", optimizer=Adam(lr=self.learning_rate))
         return model
+
+    def update_target_model(self):
+        old = self.target_model.get_weights()
+        new = self.model.get_weights()
+
+        weights = [[self.update_rate * n + (1 - self.update_rate) * o for n, o in zip(new_w, old_w)] for new_w, old_w in zip(new, old)]
+        self.target_model.set_weights(weights)
