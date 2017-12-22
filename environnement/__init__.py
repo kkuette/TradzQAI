656c74a5cba134af373e32ebcb2979ec8eeb3aa7
kkuette <awakeproduction@hotmail.fr>
2017-12-22 13:50:22 +0100

    Some fix in Environnement and added local and live env for futur use
    Former-commit-id: fefafdd1dbf8cd85f8db897da5452b2cfca42250
    Former-commit-id: 018aca34f08b51206d268daa14282687a5dde3c6
    Former-commit-id: c644e5614745fbe13e3c601708d35d6174619f09
    Former-commit-id: 56c25bb4134ed3434d5f4492cf9951cec20098bd [formerly 6bf4dcdec810653eb8faace7c60f3a4e59c9a503]
    Former-commit-id: 3d75e1190b1171a7d7d82e6edee4010a1098f59c

--- environnement/__init__.py
+++ environnement/__init__.py
@@ -1,3 +1,1 @@
 from .base_env import Environnement
-from .stock_env import Stock_Env
-from .eve_env import Eve_Env
---
+++ environnement/live_env.py
@@ -0,0 +1,6 @@
+from .base_env import Environnement
+
+class live_env(Environnement):
+
+    def __init__(self):
+        Environnement.__init__(self)
