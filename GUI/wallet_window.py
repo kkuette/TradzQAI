ec29dbeaa7fcb1adec168c7c73f9f97516b0c4f7
kkuette <awakeproduction@hotmail.fr>
2018-04-26 06:07:09 +0200

    Added candlestick chart in wallet window
    Former-commit-id: c22fbdcc3d89ee35fb46cbaebdc33e6e9e8f1695
    Former-commit-id: 56adf869063ad7a45a157ef172d5754fa2b2f4f2
    Former-commit-id: ee5bf18132b368d2f7e4b63e02bd1f9ec5d4631d [formerly 355eb37ac769144bc95b5ca650f5cf986e50374e] [formerly d8d5acd6ec81df7f9756d5b76acda3243846f0ce [formerly 5bd76e6a94d81c97c4c827391de31e1f54de555f]] [formerly 5c425f652bf52468436de4c74ab19548a0923420]
    Former-commit-id: bf61f8b71cf26ba162cf0f7d911161487e10bf2e
    Former-commit-id: 5ed1e5a765e9858b8573f7c33ca204c9d57262cc [formerly bf95282df8fbcc457412430bab94ee25a9fb9b45]
    Former-commit-id: 43737e0c6c786f772fee6391f04b54d268c8c594

--- GUI/wallet_window.py
+++ GUI/wallet_window.py
@@ -6,7 +6,25 @@ from PyQt5.QtCore import *
 
 import pyqtgraph as pg
 
+from .candlestick import CandlestickItem
+
 class WalletWindow(QWidget):
 
     def __init__(self, root, env):
         super(QWidget, self).__init__(root)
+        GB = QGridLayout(self)
+        self.init_chart()
+        GB.addWidget(self._chart, 0, 0)
+
+    def init_chart(self):
+        self.candleitem = CandlestickItem()
+        self._chart = pg.PlotWidget()
+        self._chart.addItem(self.candleitem)
+
+    def Update_chart(self, env):
+        if len(env.lst_data_full) > 1:
+            self._chart.clear()
+            self._chart.addItem(self.candleitem)
+            self.candleitem.addData(env.lst_data_full)
+            self.candleitem.generatePicture()
+            self._chart.autoRange()
