39ad05f3acdb81b266be4bf981d96e938b9ad1b8
kkuette <awakeproduction@hotmail.fr>
2018-04-27 23:40:24 +0200

    added model summary saving, training data saving for other uses, prompt running for fast training
    Former-commit-id: fa5013c0e801400aa01e6ce8735e00f5809a1718
    Former-commit-id: d9d5f5cc66658419d6fb857d80709f1a11d10a5a
    Former-commit-id: 6fa1b551c1eaff403cfd9c6fa5ceb72bdda2b9c8 [formerly 543b858a3cd0d7429c4d3db99cc34661290ff877] [formerly 49bc8e429515230cfc02175daa104eb9591a214b [formerly 59d97d1a8527e14a15ab9186cee67d8bdd3c3799]] [formerly ea6ff39ca2011d26e34d9a6faff2258f082767fe]
    Former-commit-id: 3e4f9bf43aadd214326e2457c8e27f5c64fa44fd
    Former-commit-id: 301d78c371bdf4320078fc117041939a47fc724e [formerly e65de77aa58aa056e7afc9f30c44d50aa1903120]
    Former-commit-id: d3aa88b1e2451fd2abea7478f23ed1a3d6d0df0c

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
-from PyQt5 import QtGui
-from PyQt5.QtWidgets import QApplication
-
 import os
+import sys
+
+import argparse
+parser = argparse.ArgumentParser(description="TradzQAI, all model configuration are in conf.cfg")
+parser.add_argument("-g", "--GUI", type=int, help="Display GUI, default not displaying", default=0, choices=[0, 1])
+parser.add_argument("-v", "--verbose", type=int, help="Verbosity mode, default is not verbose", default=0, choices=[0, 1])
+parser.add_argument("-m", "--mode", type=int, help="Training or eval mode, default is training. Uselfull only without GUI displayed", default=0, choices=[0, 1])
+args = parser.parse_args()
 
 # Hide TF loading logs
 #os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -9,15 +14,23 @@ import os
 # Set cpu
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-import qdarkstyle
-
-from GUI import MainWindow
-
-import sys
-
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
-    launch = MainWindow()
-    launch.show()
-    sys.exit(app.exec_())
+    if args.GUI == 1:
+        import qdarkstyle
+        from PyQt5 import QtGui
+        from PyQt5.QtWidgets import QApplication
+        from GUI import MainWindow
+
+        app = QApplication(sys.argv)
+        app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+        launch = MainWindow()
+        launch.show()
+        sys.exit(app.exec_())
+    else:
+        from environnement import Environnement
+        from core import Local_Worker
+
+        env = Environnement()
+        env.gui = 0
+        worker = Local_Worker(env)
+        worker.run()
