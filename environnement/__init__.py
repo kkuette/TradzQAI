b7f29f6759a50155e31b4faf45b26fc2644327a3
kkuette <awakeproduction@hotmail.fr>
2018-05-13 19:51:50 +0200

    cleaning
    Former-commit-id: bf3f19fcd44ff93839d43c86e3637a28d687f093
    Former-commit-id: 4d3edadd4321af3b4410edfeee8fd7a91574b6b6
    Former-commit-id: 2a4302574bf448981e3757dae776cfa41bfc9538
    Former-commit-id: 35c04cb4c74fb63d980a9800d9f950343ea7c7c9 [formerly 22b86f81b61535d1f8a246b80116f46ec21f27ee]
    Former-commit-id: 9134dd84e95a59d3df93fca9de48841b8e97a614

--- environnement/__init__.py
+++
@@ -1,2 +0,0 @@
-from .base_env import Environnement
-from .wallet import Wallet
--- environnement/live_env.py
+++
@@ -1,6 +0,0 @@
-from .base_env import Environnement
-
-class live_env(Environnement):
-
-    def __init__(self):
-        Environnement.__init__(self)
