240c42cddc3cc65e9f41b8a7991f4b89a8b067f2
kkuette <awakeproduction@hotmail.fr>
2018-05-15 22:22:17 +0200

    Updated agents
    Former-commit-id: 9a6f0db0bcc57f198d4062cce6f5893597f7e5bb
    Former-commit-id: cfb78c61e3f4d0c89a245bd6e322394e052153fb
    Former-commit-id: 1e0bc46611f4685525f55450146bed0235124f6a
    Former-commit-id: 30e3922cb7cb3066f8a4cdab502438d4bacb5f12 [formerly 19a060342c058d400048dab0875b2c50c13053ae]
    Former-commit-id: f143752d9e7a45ce38c02a7a4061d35eb5ca45b8

--- agents/PPO.py
+++ agents/PPO.py
@@ -8,11 +8,10 @@ class PPO(PPOAgent):
     def __init__(self, env=None, device=None):
         self.action_size = 3
         self.env = env
-
         PPOAgent.__init__(self,
                            states = dict(type='float', shape=env.state.shape),
                            actions = dict(type='int', num_actions=self.action_size),
-                           network = env.get_network(),
+                           network = env.settings['network'],
                            device = device,
                            discount = env.hyperparameters['gamma'],
                            batching_capacity = env.batch_size * 100,
@@ -22,16 +21,16 @@ class PPO(PPOAgent):
         self._load_model()
 
     def _save_model(self):
-        if self.env.logger.model_file_name == "":
+        if self.env.saver.model_file_name == "":
             try:
-                self.env.logger.model_file_name = self.env.model_name + "_" + self.env.stock_name.split("_")[0] + "_" + self.env.stock_name.split("_")[1]
+                self.env.saver.model_file_name = self.env.model_name + "_" + self.env.stock_name.split("_")[0] + "_" + self.env.stock_name.split("_")[1]
             except:
-                self.env.logger.model_file_name = self.env.model_name + "_" + self.env.stock_name.split("_")[0]
-            self.env.logger.model_file_path = self.env.logger.model_directory + "/" + self.env.logger.model_file_name
-        self.save_model(directory=self.env.logger.model_file_path, append_timestep=True)
+                self.env.saver.model_file_name = self.env.model_name + "_" + self.env.stock_name.split("_")[0]
+            self.env.saver.model_file_path = self.env.saver.model_directory + "/" + self.env.saver.model_file_name
+        self.save_model(directory=self.env.saver.model_file_path, append_timestep=True)
 
     def _load_model(self):
         try:
-            self.restore_model(self.env.logger.model_directory)
+            self.restore_model(self.env.saver.model_directory)
         except:
             pass
--- agents/TRPO.py
+++ agents/TRPO.py
@@ -12,7 +12,7 @@ class TRPO(TRPOAgent):
         TRPOAgent.__init__(self,
                            states = dict(type='float', shape=env.state.shape),
                            actions = dict(type='int', num_actions=self.action_size),
-                           network = env.get_network(),
+                           network = env.settings['network'],
                            device = device,
                            discount = env.hyperparameters['gamma'],
                            batching_capacity = env.batch_size * 100,
@@ -21,13 +21,13 @@ class TRPO(TRPOAgent):
         self._load_model()
 
     def _save_model(self):
-        if self.env.logger.model_file_name == "":
+        if self.env.saver.model_file_name == "":
             try:
-                self.env.logger.model_file_name = self.env.model_name + "_" + self.env.stock_name.split("_")[0] + "_" + self.env.stock_name.split("_")[1]
+                self.env.saver.model_file_name = self.env.model_name + "_" + self.env.stock_name.split("_")[0] + "_" + self.env.stock_name.split("_")[1]
             except:
-                self.env.logger.model_file_name = self.env.model_name + "_" + self.env.stock_name.split("_")[0]
-            self.env.logger.model_file_path = self.env.logger.model_directory + "/" + self.env.logger.model_file_name
-        self.save_model(directory=self.env.logger.model_file_path, append_timestep=True)
+                self.env.saver.model_file_name = self.env.model_name + "_" + self.env.stock_name.split("_")[0]
+            self.env.saver.model_file_path = self.env.saver.model_directory + "/" + self.env.saver.model_file_name
+        self.save_model(directory=self.env.saver.model_file_path, append_timestep=True)
 
     def _load_model(self):
         try:
