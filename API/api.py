1e7d2b95e46ad5be8396da6ded87436026543998
kkuette <awakeproduction@hotmail.fr>
2018-10-04 00:20:03 +0200

    Added some usefull functions and auto_cancel to close pos on stop

--- API/api.py
+++ API/api.py
@@ -4,18 +4,18 @@ class Api(object):
 
     def __init__(self, api_name="cbpro", key=None, b64=None,
         passphrase=None, product_id=['BTC-EUR'], db=None,
-        url="https://api.pro.coinbase.com", mode="maker"):
+        url="https://api.pro.coinbase.com", mode="maker",
+        auto_cancel=True):
 
         self.mode = mode
+        self.auto_cancel = auto_cancel
         self.key = key
         self.b64 = b64
         self.passphrase = passphrase
         self.db = db
         self.url = url
         self.product_id = product_id
-
         self.setAPI(api_name=api_name)
-        self.initAPI()
 
     def srcAPI(self):
         ignore = ['__init__.py', 'api.py', '__pycache__']
@@ -27,7 +27,8 @@ class Api(object):
 
     def initAPI(self):
         self._api = self._api(key=self.key, b64=self.b64, passphrase=self.passphrase,
-            product_id=self.product_id, db=self.db, url=self.url, mode=self.mode)
+            product_id=self.product_id, db=self.db, url=self.url, mode=self.mode,
+            auto_cancel=self.auto_cancel)
 
     def setAPI(self, api_name=None):
         if api_name:
@@ -35,6 +36,7 @@ class Api(object):
         if self.api_name in self.srcAPI():
             self._api = getattr(getattr(__import__('API.%s' % self.api_name),
                 self.api_name), self.api_name+'wrapper')
+            self.initAPI()
 
     def setBestPriceFunc(self, func):
         """
@@ -42,9 +44,9 @@ class Api(object):
 
             args:
                 last bids: function that take a int (depth)
-                        return: <generator> each row is a list filled with dict
+                        return: list each row is a list filled with dict
                 last asks: function that take a int (depth)
-                        return: <generator> each row is a list filled with dict
+                        return: list each row is a list filled with dict
         """
         self._api.setBestPriceFunc(func)
 
@@ -55,9 +57,18 @@ class Api(object):
         return self._api.getWallet()
 
     def getAccount(self, currency="EUR"):
-        for account in self.getAccounts()
+        for account in self.getAccounts():
             if account['currency'] == currency:
                 return account
+
+    def getHistorical(self):
+        return self._api.getHistorical()
+
+    def getEvent(self):
+        return self._api.event
+
+    def getTicks(self):
+        return self._api.getTicks()
 
     def buy(self, volume):
         self._api.addOrder("buy", volume)
